d53f10b532578029a04abb009ff4ba96da4e05fb
Anatoliy Varanitsa <avaranitsa@chegg.com>
2015-10-14 14:38:44 +0300

    fix root_url

--- app/controllers/exception_notification_server/application_controller.rb
+++ app/controllers/exception_notification_server/application_controller.rb
@@ -6,8 +6,8 @@ module ExceptionNotificationServer
 
     def redirect_to_root
       respond_to do |format|
-        format.js { render js: "window.location = '#{root_url}';" }
-        format.all { redirect_to root_url }
+        format.js { render js: "window.location = '#{Rails.application.routes.url_helpers}';" }
+        format.all { redirect_to Rails.application.routes.url_helpers }
       end
     end
 
