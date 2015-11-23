d2b7ff50ec993bdda27f2d1ed9a3f00a4f966376
Anatoliy Varanitsa <avaranitsa@chegg.com>
2015-11-23 14:22:35 +0200

    fix

--- app/models/exception_notification_server/notification.rb
+++ app/models/exception_notification_server/notification.rb
@@ -74,7 +74,7 @@ module ExceptionNotificationServer
     def graph_data(from)
       result = from.to_datetime.step(Time.zone.now.to_datetime, 1).map { |time| [time.beginning_of_day.to_i * 1000, 0] }.to_h
       childrens.each { |notification| result[notification.created_at.beginning_of_day.to_i * 1000] = result[notification.created_at.beginning_of_day.to_i * 1000].to_i + 1 if notification.created_at >= from }
-      result[created_at.beginning_of_day.to_i * 1000] += 1 if created_at >= from
+      result[created_at.beginning_of_day.to_i * 1000] = result[created_at.beginning_of_day.to_i * 1000].to_i + 1 if created_at >= from
       result
     end
 
